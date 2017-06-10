7f834fc20244673944ef6bd0d1c9c155651a4f2f
Jerry Vigil <Jerry@Jerrys-Mac-mini.local>
2017-06-10 00:39:57 -0700

    a

--- routes/index.js
+++ routes/index.js
@@ -5,24 +5,38 @@ const sqlite3 = require('sqlite3').verbose();
 const PAGE_TITLE = 'Growth Shares'
 const DB_FILE_NAME = 'morningstar_data.sqlite3';
 
+function getHrTimeDiffMilliseconds(startTime, endTime) {
+    return (endTime[0] - startTime[0])*1000 + (endTime[1] - startTime[1])/1e6;
+}
+
+function fetchTickersListFromDb(response) {
+    var tickerRows = [];
+    var start = process.hrtime();
+    var db = new sqlite3.Database(DB_FILE_NAME);
+    db.all('SELECT DISTINCT ticker, company_name, exchange FROM ticker_list ORDER BY ticker ASC', (err, rows) => {
+        for (var i=0; i<rows.length; i++) {
+            tickerRows.push({
+                'ticker': rows[i].ticker,
+                'companyName': rows[i].company_name,
+                'exchange': rows[i].exchange.charAt(0).toUpperCase() + rows[i].exchange.slice(1)
+            });
+        }
+        db.close();
+        var end = process.hrtime(); 
+        console.log('Read %d ticker symbols from the database in %f ms.', i,
+            getHrTimeDiffMilliseconds(start, end));
+        //response.json(tickerRows);
+        response.render('ticker_list', {'tickerRows': tickerRows});
+    });
+}
+
 /* GET home page. */
 router.get('/', (request, response, next) => {
-	response.render('index', { title: PAGE_TITLE });
+    response.render('index', { title: PAGE_TITLE });
 });
 
-router.get('/ticker_list',  (request, response, next) => {
-	var tickerList = [];
-	var db = new sqlite3.Database(DB_FILE_NAME);
-	db.all('SELECT DISTINCT ticker, company_name FROM ticker_list ORDER BY ticker ASC', (err, rows) => {
-		var ticker_count = 0;
-		for (row of rows) {
-			tickerList.push(`${row.ticker}, ${row.company_name}`);
-		}
-		console.log('Read %d ticker symbols from the database.', ticker_count);
-		db.close();
-		var responseStr = tickerList.join('<br>');
-		response.send(responseStr);
-	});
+router.get('/ticker_list', (request, response, next) => {
+    fetchTickersListFromDb(response);
 });
 
 module.exports = router;
